ba42e350e35f1c15f5386234b7bc2e0f60f5470e
Naral <77269201+NaralC@users.noreply.github.com>
2022-11-21 17:04:22 +0000

    Implemented barebones routing + NotFoundPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,19 @@ import "./App.css";
 import AccountPage from "./pages/AccountPage";
 import LoginPage from "./pages/LoginPage";
 import GraduatePage from "./pages/GraduatePage";
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App: React.FC = () => {
-
   // Implement routing later
   return (
     <div className="bg-white">
-      <GraduatePage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/graduate" element={<GraduatePage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 };
---
+++ client/src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type NotFoundProps = {}
+
+const NotFoundPage: React.FC<NotFoundProps> = () => {
+  return (
+    <div className='text-5xl'>NotFoundPage</div>
+  )
+}
+
+export default NotFoundPage;
