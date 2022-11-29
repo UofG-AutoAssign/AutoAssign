6914bfacaa27c4ccd6a1958c1c0cbf15c3199a13
Vaughn <257525m@student.gla.ac.uk>
2022-11-29 15:10:35 +0000

    Modified graduate and form fields, added correct relationships.

--- AutoAssign/AutoAssign/settings.py
+++ AutoAssign/AutoAssign/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = "AutoAssign.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "newdatabase",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
