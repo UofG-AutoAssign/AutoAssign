b2007a3706c8f7420b6934ffe04f29d73cb8bfc0
Max Wang <2561238w@student.gla.ac.uk>
2022-11-30 11:05:40 +0000

    implement Login Test Close(#43)

--- AutoAssign/Assign/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-
-#Unit Test
-
-#Test DataBase
--- AutoAssign/Assign/urls.py
+++ AutoAssign/Assign/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, re_path
 from Assign import views
 
-
 app_name = 'Assign'
 
 urlpatterns = [
-    path('', views.LoginView.as_view()),     # CBV Style Login
-    path('home/hr/', views.HrView.as_view()),
-    path('home/grad/', views.GradView.as_view()),
-    path('home/man/', views.ManView.as_view()),
-    path('home/hr/create', views.HrViewCreate.as_view()),
+    path('', views.LoginView.as_view(), name="Login"),  # CBV Style Login
+    path('home/hr/', views.HrView.as_view(), name="HrHome"),
+    path('home/grad/', views.GradView.as_view(), name="GradHome"),
+    path('home/man/', views.ManView.as_view(), name="ManHome"),
+    path('home/hr/create', views.HrViewCreate.as_view(), name="HrCreate"),
 ]
