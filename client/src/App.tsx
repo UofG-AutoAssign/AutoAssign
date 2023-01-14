980ade5b8c89abba5e8e47885a92f2be0f69bb35
Miles <1978176436@qq.com>
2023-01-14 14:25:23 +0000

    assign + confirm popups for HR pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,10 @@ import ForgotPassPage from "./pages/ForgotPassPage";
 import PrivateRoute from "./utility/PrivateRoute";
 import AuthContextProvider from "./context/AuthContextProvider";
 import HRhomePage from "./pages/HRhomePage";
+import PopupPage from "./pages/PopupPage";
 import PreferencePage from "./pages/PreferencePage";
+import ComfirmPopup from "./pages/ComfirmPopup";
+import AssignPopup from "./pages/AssignPopup";
 
 const App: React.FC = () => {
   // Implement routing later
@@ -32,7 +35,10 @@ const App: React.FC = () => {
         <Route path="/hr" element={<HRhomePage/>} />
         <Route path="/*" element={<NotFoundPage />} />
         <Route path="/forgot_password" element={<ForgotPassPage />} />
+        <Route path= "/creatACpop" element = {<PopupPage/>}/>
         <Route path="/preference_page" element={<PreferencePage />} />
+        <Route path="/comfirmPop" element={<ComfirmPopup/>} />
+        <Route path="/assignPop" element={<AssignPopup/>} />
       </Routes>
       </AuthContextProvider>
     </div>
