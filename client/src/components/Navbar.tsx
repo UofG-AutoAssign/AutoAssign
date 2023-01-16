8344dcbb021e6b4b6fd749b1dcdc1945dcbc6dfb
Miles <1978176436@qq.com>
2023-01-16 14:03:20 +0000

    BackButton

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,25 +4,40 @@ import { FcSettings, FcPicture } from "react-icons/all";
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { DiReact } from "react-icons/all";
+import { useNavigate } from "react-router-dom";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const authContext = useContext(AuthContext);
+const authContext = useContext(AuthContext);
+
+const backButton = (): JSX.Element => {
+    let navigate = useNavigate();
+    return (
+      <>
+        <button className="btn bg-blue-800  text-btnColor2" onClick={() => navigate(-1)}>Back</button>
+      </>
+    );
+  };
+
+
 
   useEffect(() => {
     authContext?.getUserInfo();
 
     return () => {
       // Clean up here
-    }
-  }, [])
-  
+    };
+  }, []);
 
   return (
     <div className="navbar bg-white text-black px-5 py-3 mb-5 border-b-slate-200 border-2 flex flex-col justify-between sm:flex-row">
       <div className="">
         {/* <DiReact className="text-teal-400 text-5xl hover:animate-spin"/> */}
+
+        {/* <button className="btn">Button</button> */}
+        {backButton()}
+
         <img className="max-w-lg hover:animate-spin" src={logo} />
         <div className="btn btn-ghost normal-case text-xl mx-10">
           AutoAssign
