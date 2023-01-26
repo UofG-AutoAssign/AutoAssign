2d3e2d0042d1a00f6bbaf3fcff3d2986df2e8a3c
Naral <77269201+NaralC@users.noreply.github.com>
2023-01-26 00:29:13 +0000

    dark theme for nav bar

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
+  darkMode: "false",
   theme: {
     extend: {},
     colors:{
