bfc19e46cba085b4a800d33da481775fc955ae89
Max Wang <2561238w@student.gla.ac.uk>
2023-02-10 20:38:12 +0000

    Update Url

--- AutoAssign/Assign/urls.py
+++ AutoAssign/Assign/urls.py
@@ -5,15 +5,15 @@ app_name = 'Assign'
 
 urlpatterns = [
     path('', views.LoginView.as_view(), name="Login"),  # CBV Style Login
-    path('home/hr/', views.HrView.as_view(), name="HrHome"),
-    path('home/hr/CreateTeam', views.CreateTeamView.as_view(), name="CreateTeam"),
     path('home/grad/', views.GradView.as_view(), name="GradHome"),
     path('home/grad/Form', views.FormView.as_view(), name="FromDisplay"),
     path('home/man/', views.ManView.as_view(), name="ManHome"),
     path('home/man/Team', views.TeamMemberView.as_view(), name="TeamMemberView"),
     path('home/man/Team/Setting', views.TeamSettingView.as_view(), name="TeamSettingView"),
     path('home/man/Team/UpdateSetting', views.TeamSettingView.as_view(), name="UpdateTeamSetting"),
+    path('home/hr', views.HrView.as_view(), name="HrHome"),
     path('home/hr/create', views.HrViewCreate.as_view(), name="HrCreate"),
+    path('home/hr/CreateTeam', views.CreateTeamView.as_view(), name="CreateTeam"),
     path('home/hr/TeamView', views.AllTeamView.as_view(), name="AllTeam"),
     path('home/hr/GradView', views.AllGradView.as_view(), name="AllGrad"),
     path('home/hr/DeleteGrad', views.DeleteGrad.as_view(), name="DeleteGrad"),
