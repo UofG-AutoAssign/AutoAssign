c370f6eb19d9706c04d7f6db0fab206e0a729d56
Max Wang <2561238w@student.gla.ac.uk>
2023-02-11 12:58:40 +0000

    Test ManagerModel (issue #101)

--- AutoAssign/Assign/test/test_database.py
+++ AutoAssign/Assign/test/test_database.py
@@ -18,4 +18,14 @@ class HrTestCase(TestCase):
         self.assertEqual(models.HR.objects.count(), 1)
         self.assertEqual(models.HR.objects.get().email, "Hr@email.com")
 
+class ManagerTestCase(TestCase):
 
+    def setUp(self):
+        models.Manager.objects.create(email='Manager@email.com',
+                                 first_name='Manager', second_name='Test',
+                                 password='123456', role='2')
+
+    def test_my_model(self):
+        # Ensure the my_model object has been created in the cache database
+        self.assertEqual(models.Manager.objects.count(), 1)
+        self.assertEqual(models.Manager.objects.get().email, "Manager@email.com")
