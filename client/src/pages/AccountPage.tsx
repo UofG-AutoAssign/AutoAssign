3397a39cd31cfe8c17290c9c2c51e9eba0ce1dc2
Naral <77269201+NaralC@users.noreply.github.com>
2023-02-19 14:53:25 +0000

    Removed hint texts (Closes #118)

--- client/src/pages/AccountPage.tsx
+++ client/src/pages/AccountPage.tsx
@@ -36,7 +36,7 @@ const AccountPage: FC = () => {
                 type="text"
                 id="first_name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="e.g. 123Xxxxx"
+                placeholder="Current Password"
                 required
               ></input>
             </div>
@@ -51,7 +51,7 @@ const AccountPage: FC = () => {
                 type="text"
                 id="last_name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="e.g. 123Xxxxx"
+                placeholder="New Password"
                 required
               ></input>
             </div>
@@ -67,7 +67,7 @@ const AccountPage: FC = () => {
                 type="text"
                 id="last_name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="e.g. 123Xxxxx"
+                placeholder="Confirm New Password"
                 required
               ></input>
             </div>
