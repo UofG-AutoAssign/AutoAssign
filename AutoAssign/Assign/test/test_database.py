280ff3695d44637a3f933f3777855487ea5dc221
Miles <1978176436@qq.com>
2023-03-01 16:01:59 +0000

    Create mock data for testing (Closes #101)
    Created mock data for unit testing. The data includes
    
    - HR, Manager, Team, Skill, Department, Graduate

--- AutoAssign/Assign/test/test_database.py
+++ AutoAssign/Assign/test/test_database.py
@@ -14,7 +14,6 @@ class HrTestCase(TestCase):
                                  password='123456', role='3')
 
     def test_my_model(self):
-        # Ensure the my_model object has been created in the cache database
         self.assertEqual(models.HR.objects.count(), 1)
         self.assertEqual(models.HR.objects.get().email, "Hr@email.com")
 
@@ -27,6 +26,75 @@ class ManagerTestCase(TestCase):
                                       password='123456', role='2')
 
     def test_my_model(self):
-        # Ensure the my_model object has been created in the cache database
         self.assertEqual(models.Manager.objects.count(), 1)
         self.assertEqual(models.Manager.objects.get().email, "Manager@email.com")
+
+
+class Graduate(TestCase):
+
+    def setUp(self):
+        models.Graduate.objects.create(email='Graduate@email.com',
+                                       first_name='Graduate', second_name='Test',
+                                       password='123456', role='3')
+
+    def test_my_model(self):
+        self.assertEqual(models.Graduate.objects.count(), 1)
+        self.assertEqual(models.Graduate.objects.get().email, "Graduate@email.com")
+
+
+class Department(TestCase):
+
+    def setUp(self):
+        models.Department.objects.create(depart_name='Department')
+
+    def test_my_model(self):
+        self.assertEqual(models.Department.objects.count(), 1)
+        self.assertEqual(models.Department.objects.get().depart_name, "Department")
+
+
+class HR(TestCase):
+
+    def setUp(self):
+        models.HR.objects.create(email='HR@email.com',
+                                 first_name='HR', second_name='Test',
+                                 password='123456', role='1')
+
+    def test_my_model(self):
+        self.assertEqual(models.HR.objects.count(), 1)
+        self.assertEqual(models.HR.objects.get().email, "HR@email.com")
+
+
+class Form(TestCase):
+
+    def setUp(self):
+        Skill_obj = models.Skill.objects.create(skill_name="1")
+        models.Form.objects.create(interest=1, experience=3, Skill_id=Skill_obj, )
+
+    def test_my_model(self):
+        self.assertEqual(models.Form.objects.count(), 1)
+        self.assertEqual(models.Form.objects.get().interest, 1)
+
+
+class Team(TestCase):
+
+    def setUp(self):
+        models.Skill.objects.create(skill_name="1")
+        Team = models.Team.objects.create(team_name="test", ratio=0.5)
+
+        Skill_obj = models.Skill.objects.all()
+
+        Team.Skill.set(Skill_obj)
+
+    def test_my_model(self):
+        self.assertEqual(models.Form.objects.Team(), 1)
+        self.assertEqual(models.Form.objects.get().team_name, "test")
+
+
+class Skill(TestCase):
+
+    def setUp(self):
+        models.Skill.objects.create(skill_name="1")
+
+    def test_my_model(self):
+        self.assertEqual(models.Skill.objects.Team(), 1)
+        self.assertEqual(models.Skill.objects.get().team_name, "1")
