3d34b3e188b2dddd503dd1bf12f941ee3d1ef31e
Naral <77269201+NaralC@users.noreply.github.com>
2023-03-07 12:22:43 +0000

    Skeleton for code route authentication  (For #147)
    - Uses url to authenticate with usertype
    - (In the future) use a JWT for extra validation

---
+++ client/src/components/RequireAuth.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import useAuth from "../hooks/useAuth";
+import { useLocation, Outlet, Navigate, useNavigate } from "react-router-dom";
+
+const RequireAuth: FC = () => {
+  const { authToken, userType } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const userTypeMatchesUrl = (): boolean => {
+    const query = location.pathname.split("/").at(1)?.toLowerCase();
+
+    if (query === "account") return true;
+
+    if (query !== userType.toLowerCase()) {
+        console.log("bruh what u doing here");
+        navigate("/unauthorized");
+    }
+    return true;
+  };
+
+  // @Todo use JWT to autheticate
+
+  return userTypeMatchesUrl() === true ? (
+    <Outlet />
+  ) : (
+    <Navigate to={"/"} replace />
+  );
+};
+
+export default RequireAuth;
