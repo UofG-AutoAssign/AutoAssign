30079c47b2131c8c78d2404b9f570fd93986011a
Naral <77269201+NaralC@users.noreply.github.com>
2023-03-11 18:00:34 +0000

    Removed the route for HR's test component
    Co-authored-by: Binhao Li
    
    - also fixed "clicking logo in account page doesn't bring you back to menu"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,6 @@ import { observer } from "mobx-react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import RequireAuth from "./components/RequireAuth";
-import TestPageForHRTable from "./pages/TestPageForHRTable";
 
 const App: FC = observer(() => {
   return (
@@ -32,7 +31,6 @@ const App: FC = observer(() => {
           <Route path="forgot_password" element={<ForgotPassPage />} />
           <Route path="/sign_up/:signUpToken" element={<SignUpPage />} />
           <Route path="*" element={<NotFoundPage />} />
-          <Route path="/test" element={<TestPageForHRTable />} />
 
           {/* Private Routes */}
           <Route element={<RequireAuth />}>
