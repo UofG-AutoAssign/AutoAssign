0261bb59b5606490007ac52385290cb1ef855f38
Max Wang <2561238w@student.gla.ac.uk>
2023-03-12 21:35:19 +0000

    Rewite backend by pyline (Close #178)

--- AutoAssign/ext/auth.py
+++ AutoAssign/ext/auth.py
@@ -1,13 +1,28 @@
+"""
+Authentication component that verifies user identity,
+and determines whether they have the appropriate permissions.
+"""
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import exceptions
 
+# pylint: disable=E0401
 from Assign import models
 
+# pylint: disable=E0401
 from ext.jwt_auth import parse_payload
 
 
-def locateUser(payload):
+def locate_user(payload):
+    """
+        Locate the user in the database based on the given payload.
+
+        Args:
+            payload (dict): A dictionary containing the user data.
+
+        Returns:
+            The user object in the database.
+    """
     data = payload['data']
     user = data['email']
 
@@ -17,30 +32,33 @@ def locateUser(payload):
 
     if manger_object:
         return manger_object
-    elif hr_object:
+    if hr_object:
         return hr_object
-    elif grad_object:
+    if grad_object:
         return grad_object
+
+    return None
 
 
 class JwtQueryParamAuthentication(BaseAuthentication):
     """
-   The user needs to transfer the parameters in the url to transfer tokens"""
+    The user needs to transfer the parameters in the url to transfer tokens, for example:
+    """
 
     def authenticate(self, request):
         token = request.query_params.get('token')
         payload = parse_payload(token)
         if not payload['status']:
-            return
+            return None
 
-        user_object = locateUser(payload)
+        user_object = locate_user(payload)
         return user_object, token
 
 
 class JwtAuthorizationAuthentication(BaseAuthentication):
     """
-       Users need to transfer tokens in the form of request headers
-       """
+       Users need to transfer tokens in the form of request headers, for example:
+    """
 
     def authenticate(self, request):
         # Verification token is required for non-login pages
@@ -49,11 +67,14 @@ class JwtAuthorizationAuthentication(BaseAuthentication):
         if not payload['status']:
             raise exceptions.AuthenticationFailed(payload)
 
-        user_object = locateUser(payload)
+        user_object = locate_user(payload)
         return user_object, token
 
 
 class NoAuthentication(BaseAuthentication):
+    """
+        Unauthorized
+    """
 
     def authenticate(self, request):
         raise AuthenticationFailed({"status": False, 'msg': "Authentication failed"})
--- AutoAssign/ext/per.py
+++ AutoAssign/ext/per.py
@@ -1,11 +1,32 @@
+"""
+Confirm user permissions
+"""
 from rest_framework.permissions import BasePermission
 
 
 # Permissions
 class GradPermission(BasePermission):
+    """
+        Custom permission class to check whether the user
+        -making the request has the required permissions
+        -to access the resource.
+
+        The user must have a role equal to 1 to have permission to access the resource.
+    """
     message = {"status": False, 'msg': "Require Graduate's Permission"}
 
     def has_permission(self, request, view):
+        """
+           Check whether the user making the request has
+           -the required permissions to access the resource.
+
+           Args:
+               request (Request): The HTTP request object.
+               view (View): The view handling the request.
+
+           Returns:
+               bool: True if the user has the required permissions, False otherwise.
+       """
         if request.user.role == 1:
             return True
 
@@ -13,9 +34,27 @@ class GradPermission(BasePermission):
 
 
 class ManagerPermission(BasePermission):
+    """
+       Custom permission class to check whether the user
+       -making the request has the required permissions
+       -to access the resource.
+
+       The user must have a role equal to 2 to have permission to access the resource.
+   """
     message = {"status": False, 'msg': "Require Manager's Permission"}
 
     def has_permission(self, request, view):
+        """
+            Check whether the user making the request has
+            -the required permissions to access the resource.
+
+            Args:
+                request (Request): The HTTP request object.
+                view (View): The view handling the request.
+
+            Returns:
+                bool: True if the user has the required permissions, False otherwise.
+        """
         if request.user.role == 2:
             return True
 
@@ -23,9 +62,28 @@ class ManagerPermission(BasePermission):
 
 
 class HrPermission(BasePermission):
+    """
+      Custom permission class to check whether the user
+      -making the request has the required permissions
+      -to access the resource.
+
+      The user must have a role equal to 3 to have permission to access the resource.
+    """
     message = {"status": False, 'msg': "Require Hr's Permission"}
 
     def has_permission(self, request, view):
+        """
+            Check whether the user making the request
+            has the required permissions
+            to access the resource.
+
+            Args:
+                request (Request): The HTTP request object.
+                view (View): The view handling the request.
+
+            Returns:
+                bool: True if the user has the required permissions, False otherwise.
+        """
         if request.user.role == 3:
             return True
 
