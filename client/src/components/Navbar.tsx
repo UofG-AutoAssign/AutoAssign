876d89374b5fcb387bfe47853bad7c34a59b8874
Hugo Findlay <2554911f@student.gla.ac.uk>
2023-03-13 15:34:05 +0000

    Added logo to page (Closes #main 171)
    the commit replaces the react Icon with our own logo
    
     - new logo on the navbar
     - new tab icon
     - navbar logo now redirects to the user landingpage

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,19 @@
 import { useEffect, FC } from "react";
 import { BsFillMoonStarsFill, BsSun } from "react-icons/all";
-import { Link } from "react-router-dom";
-import { RiReactjsFill } from "react-icons/ri";
+import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import { MdOutlineSwitchAccount } from "react-icons/md";
 import themeStore from "../context/themeStore";
+import logo from "../assets/logo.svg"
 
 const Navbar: FC<{
   hideLogoutButton?: boolean;
   hideAccountButton?: boolean;
 }> = ({ hideLogoutButton, hideAccountButton }) => {
-  const routerNavigator = useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
 
   const BackButton = (): JSX.Element => {
     return (
@@ -19,12 +21,13 @@ const Navbar: FC<{
         <div className="tooltip tooltip-bottom" data-tip="Go Back">
           <IoIosArrowBack
             className="text-3xl hover:scale-125"
-            onClick={() => routerNavigator(-1)}
+            onClick={() => navigate(-1)}
           />
         </div>
       </>
     );
   };
+
 
   const ThemeToggleButton = (): JSX.Element => {
     return (
@@ -42,8 +45,17 @@ const Navbar: FC<{
     );
   };
 
+  const goBackToLandingPage = () => {
+    // const query = location.pathname.split("/").at(1)?.toLowerCase();
+    const query = sessionStorage.getItem("userType") || "";
+    console.log(query);
+
+    if (query) navigate(`/${query}`);
+
+  }
+
   useEffect(() => {
-    return () => {};
+    return () => { };
   }, []);
 
   return (
@@ -51,8 +63,8 @@ const Navbar: FC<{
       <div className="navbar bg-gray-100 text-black dark:bg-gray-600 dark:text-white px-7 py-3 border-b-slate-200 flex flex-col justify-between sm:flex-row transition-all duration-150 shadow-xl">
         <div className="gap-5 scale-90 sm:scale-100">
           <BackButton />
-          <RiReactjsFill className="max-w-lg hover:animate-spin text-5xl text-teal-400 dark:text-teal-800 saturate-200" />
-          <div className="btn btn-ghost normal-case text-xl">AutoAssign</div>
+          <img src={logo} alt="the Auto Assign Logo" className="w-16 hover:animate-spin" onClick={() => goBackToLandingPage()} />
+          <div onClick={() => goBackToLandingPage()} className="btn btn-ghost normal-case text-xl">AutoAssign</div>
         </div>
         <div className="gap-10 text-3xl">
           <ThemeToggleButton />
@@ -67,7 +79,7 @@ const Navbar: FC<{
                 sessionStorage.removeItem("authToken");
                 sessionStorage.removeItem("userType");
                 sessionStorage.removeItem("username");
-                routerNavigator("/");
+                navigate("/");
               }}
               className="btn normal-case bg-blue-600 btn-outline dark:bg-red-500 border-0 text-white rounded-xl w-auto"
             >
