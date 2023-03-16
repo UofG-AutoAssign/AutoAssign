02ead1fca2598ec8272ad54c6b8ec2b98a240959
Naral <77269201+NaralC@users.noreply.github.com>
2023-03-16 20:21:14 +0000

    Test case for landing button link!

---
+++ client/src/__tests__/LandingButtonLink.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import LandingButtonLink from "../components/LandingButtonLink";
+import { MemoryRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom";
+import React from "react";
+
+it("AvatarBar", () => {
+  render(
+    <Router>
+      <LandingButtonLink
+        title="HR Home Page"
+        link={"/"}
+        initialState="Delete Team"
+        btn_color="bg-red-500"
+        desc="Hello this is a placeholder text"
+      />
+    </Router>
+  );
+  const buttonTitle = screen.getByText("HR Home Page");
+  const description = screen.getByText("Hello this is a placeholder text");
+  expect(buttonTitle).toBeInTheDocument();
+  expect(description).toBeInTheDocument();
+});
